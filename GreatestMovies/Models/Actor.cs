9d4ce0f40a845fd7e96b2f30094170b3e2af1386
Adam King <adam81king@gmail.com>
2017-07-26 15:54:35 -0400

    added actor model

---
+++ GreatestMovies/Models/Actor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace GreatestMovies.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int ActorID { get; set; }
+
+        public string ActorName { get; set; }
+        public DateTime DoB { get; set; }
+        public string Nationality { get; set; }
+        public string Gender { get; set; }
+
+        [ForeignKey("Movie")]
+        public string MovieID { get; set; }
+
+        ICollection<Movie> Movies { get; set; }
+    }
+}
